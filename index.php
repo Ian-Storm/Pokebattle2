5657380b4cf37d2d8c8c16bedc4140b6b6df223b
Ian Storm <99039737@mydavinci.nl>
2019-03-19 14:31:52 +0100

    reading it and bug fix

--- index.php
+++ index.php
@@ -10,3 +10,10 @@ require 'Charmeleon.php';
 
 $Pikachu = new Pikachu('Pikachu');
 $Charmeleon = new Charmeleon('Charmeleon');
+
+echo "<pre>";
+print_r($Pikachu);
+print_r($Charmeleon);
+
+echo 'Pikachu: ' . $Pikachu->health . ' health' . '<br>';
+echo 'Charmeleon: ' . $Charmeleon->health . ' health' . '<br>';
