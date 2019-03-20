92982decb5df0150d1a9be14d050cc6021777516
Ian Storm <99039737@mydavinci.nl>
2019-03-20 22:17:33 +0100

    made them attack each other

--- Pokemon.php
+++ Pokemon.php
@@ -19,4 +19,26 @@ class Pokemon {
         $this->resistance = $resistance;
 	 }
 
+	 public function __toString() {
+        return json_encode($this);
+    }
+
+    function attack($attack, $other){
+    	$dmg = $attack->dmg;
+
+    	//if type attacker = weakness other then dmg x weakness multiplier
+    	if ($this->type->type == $other->weakness->type) {
+    	//dmg x weakness multiplier
+    		$dmg = $attack->dmg * $other->weakness->multiplier;
+    	} elseif ($this->type->type == $other->resistance->type) {
+    		//dmg x resistance 
+    		$dmg = $attack->dmg - $other->resistance->value;
+    	}  
+    	$this->calculation($dmg, $other);
+    } 
+
+    function calculation($dmg, $other){
+    	$other->health -= $dmg;
+    }
+
 }
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
 <?php
 
 require 'Pokemon.php';
@@ -11,9 +15,17 @@ require 'Charmeleon.php';
 $Pikachu = new Pikachu('Pikachu');
 $Charmeleon = new Charmeleon('Charmeleon');
 
-echo "<pre>";
-print_r($Pikachu);
-print_r($Charmeleon);
+//print_r($Pikachu);
+//print_r($Charmeleon);
 
 echo 'Pikachu: ' . $Pikachu->health . ' health' . '<br>';
 echo 'Charmeleon: ' . $Charmeleon->health . ' health' . '<br>';
+echo 'Pikachu used ' . $Pikachu->attacks[1]->attack . '<br>';
+	$Pikachu->attack($Pikachu->attacks[1], $Charmeleon);
+echo 'Charmeleon: ' . $Charmeleon->health . ' health left' . '<br>';
+	 $Charmeleon->attack($Charmeleon->attacks[1], $Pikachu);
+echo 'Charmeleon used ' . $Charmeleon->attacks[1]->attack . '<br>';
+echo 'Pikachu: ' . $Pikachu->health . ' health left';
+
+?>
+</body>
